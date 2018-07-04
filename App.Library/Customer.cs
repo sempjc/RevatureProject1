b1d5fc46e2d099c9b44c7824e2e1e0e553da8a6b
Jean Carlo Semprit Rodriguez <jc.semprit@hotmail.com>
2018-07-04 19:32:43 -0400

    Rename Project to PizzaStoreApplication and subfolders of the application to App, also namespace now is app

---
+++ App.Library/Customer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace App.Library
+{
+    public class Customer
+    {
+        public string FirstName 
+        { 
+            get 
+            {
+                return FirstName; 
+            }
+
+            set
+            {
+                try
+                {
+                    IsEmptyString(value);
+                    FirstName = value;
+                }
+                catch(ArgumentException e )
+                {
+                    PrintExceptionMsg(e);
+                }
+            } 
+        }
+
+        public string LastName  
+        { 
+            get
+            {
+                return LastName; 
+            }
+
+            set 
+            {
+               try
+                {
+                    IsEmptyString(value);
+                    LastName = value;
+                }
+                catch (ArgumentException e)
+                {
+                    PrintExceptionMsg(e);
+                }
+            }
+        }
+
+        public string PreferedLocation 
+        { 
+            get
+            {
+                return PreferedLocation;
+            }
+
+            set
+            {
+                try
+                {
+                    IsEmptyString(value);
+                    PreferedLocation = value;
+                }
+                catch (ArgumentException e)
+                {
+                    PrintExceptionMsg(e);
+                }
+            }
+        }
+
+        public DateTime LastTimeOrdered { get; set; }
+
+        private Boolean IsEmptyString(string s)
+        {
+            if( s == "")
+            {
+                throw new ArgumentException(
+                    String.Format($"Empty value is not valid"), s
+                );
+            }
+            return true;
+        }
+
+        private void PrintExceptionMsg(Exception e) 
+        {
+            Console.WriteLine($"{e.GetType().Name}: {e.Message}");
+        }
+    }
+}
---
+++ App.UnitTest/Test.Library.cs
@@ -0,0 +1,124 @@
+using System;
+using App.Library;
+using Xunit;
+
+namespace App.UnitTest
+{
+    /* 
+     * TODO:
+     * 
+     * Customer:
+     * 
+     * Test Customer firstName, lastName, preferedLocation setters & getter
+     *      1. Look for test empty string
+     *      2. Look for test of data types different to string
+     * 
+     * Note: This is the only fields we need the customer provide us with 
+     * information other fields (if any) will be filled programaticaly 
+     * like 'lastOrderTime'.
+     * 
+    */ 
+    public class UnitTest1
+    {
+        // Customer test
+        // When everything is beautiful...
+        [Fact]
+        public void CustomerShouldPassFirstNameAssigment()
+        {
+            // Arrange
+            string firstName = "Jean";
+
+            Customer customer = new Customer
+            {
+                FirstName = firstName
+            };
+
+            // Act
+            var actual = customer.FirstName;
+
+            // Assert
+            Assert.Equal(firstName, actual);
+        }
+
+        // Customer test
+        // When everything is beautiful...
+        [Fact]
+        public void CustomerShouldPassLastNameAssigment()
+        {
+            // Arrange
+            string lastName = "Semprit";
+
+            Customer customer = new Customer
+            {
+                LastName = lastName
+            };
+
+            // Act
+            var actual = customer.LastName;
+
+            // Assert
+            Assert.Equal(lastName, actual);
+        }
+
+        // Customer test
+        // When everything is beautiful...
+        [Fact]
+        public void CustomerShouldPassPreferedLocationAssigment()
+        {
+            // Arrange
+            string preferedLocation = "Reston";
+
+            Customer customer = new Customer
+            {
+                PreferedLocation = preferedLocation
+            };
+
+            // Act
+            var actual = customer.PreferedLocation;
+
+            // Assert
+            Assert.Equal(preferedLocation, actual);
+        }
+
+        // Customer test
+        // When everything is beautiful...
+        [Fact]
+        public void CustomerShouldPassLastTimeOrderedAssigment()
+        {
+            // Arrange
+            DateTime lastTimeOrdered = new DateTime();
+
+            Customer customer = new Customer
+            {
+                LastTimeOrdered = lastTimeOrdered.Date
+            };
+
+            // Act
+            var actual = customer.LastTimeOrdered;
+
+            // Assert
+            Assert.Equal(lastTimeOrdered, actual);
+        }
+
+        // Customer test
+        // When everything is beautiful...
+        [Fact]
+        public void CustomerShouldNotAcceptEmptyString()
+        {
+            /*
+            TODO
+            Ask Nick about testing if code catch the error
+
+            // Arrange
+            string firstName = "";
+            Customer customer = new Customer();
+
+            // Act
+            void action() => customer.FirstName = firstName;
+
+            // Assert
+            Assert.Throws<ArgumentException>( "FirstName", action);
+            */
+        }
+    }
+}
